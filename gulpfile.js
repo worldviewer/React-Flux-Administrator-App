ece7556ddaccbfe2a922aff95d1e72e47f4c1ee4
Chris Reeve <paradigmsareconstructed@gmail.com>
2015-09-28 16:32:04 -0700

    Added 2 very basic React components: About and Homepages

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,24 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); // Runs a local dev server
 var open = require('gulp-open'); // Opens a URL in a browser
+var browserify = require('browserify'); // Bundles JS
+var reactify = require('reactify'); // Transforms React JSX to JS
+var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
+var concat = require('gulp-concat'); // concatenates files
+var lint = require('gulp-eslint'); // Lint JS files, including JSX
 
 var config = {
-	port: '9000',
+	port: '9000', // Note the quotes; this is absolutely necessary, or will not work!
 	devBaseUrl: 'http://localhost',
 	paths: {
 		html: './src/*.html',
-		dist: './dist' 
+		js: './src/**/*.js', // ** looks in subdirectories too
+		dist: './dist',
+		mainJs: './src/main.js',
+		css: [
+			'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+		]
 	}
 }
 
@@ -26,7 +37,7 @@ gulp.task('connect', function() {
 // Note that this open() command has to be JUST right to work ...
 
 gulp.task('open', ['connect'], function() {
-	gulp.src('dist/index.html').pipe(open({uri: 'http://localhost:9000', app: 'Google Chrome'}));
+	gulp.src('dist/index.html').pipe(open({uri: config.devBaseUrl + ':' + config.port, app: 'Google Chrome'}));
 });
 
 gulp.task('html', function() {
@@ -35,8 +46,33 @@ gulp.task('html', function() {
 	 	 .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-	gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function() {
+	browserify(config.paths.mainJs)
+		.transform(reactify)
+		.bundle()
+		.on('error', console.error.bind(console))
+		.pipe(source('bundle.js'))
+		.pipe(gulp.dest(config.paths.dist + '/scripts'))
+		.pipe(connect.reload());
+})
+
+gulp.task('css', function() {
+	gulp.src(config.paths.css)
+		.pipe(concat('bundle.css'))
+		.pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('lint', function() {
+	return gulp.src(config.paths.js)
+		.pipe(lint({configFile: 'eslint.config.json'}))
+		// .pipe(lint())
+		.pipe(lint.format());
+});
+
+gulp.task('watch', function() {
+	gulp.watch(config.paths.html, ['html']);
+	gulp.watch(config.paths.js, ['js', 'lint']); // lints each time that javascript changes
+	gulp.watch(config.paths.css, ['css']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,31 @@
+// Defining jQuery globally is necessary because this is where Bootstrap expects to find it
+$ = jQuery = require('jquery');
+
+var React = require('react');
+var Home = require('./components/homePage.js');
+var About = require('./components/about/aboutPage.js');
+
+var App = React.createClass({
+	render: function() {
+		var Child;
+
+		switch (this.props.route) {
+			case 'about': Child = About; break;
+			default: Child = Home;
+		}
+
+		return (
+			<div>
+				<Child/>
+			</div>
+		);
+	}
+});
+
+function render() {
+	var route = window.location.hash.substr(1);
+	React.render(<App route={route} />, document.getElementById('app'));
+}
+
+window.addEventListener('hashchange', render);
+render();
